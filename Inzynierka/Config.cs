e8f100cbca23695d5642b04b2abffdba5ece7240
michaloos <misialek1998@gmail.com>
2021-12-14 22:06:28 +0100

    config.cs oraz setting window added

---
+++ Inzynierka/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Inzynierka
+{
+    public class Config
+    {
+        public string language { get; set; }
+        public int fontSize { get; set; }
+        public string fontFamily { get; set; }
+        public Brush fontColor { get; set; }
+        public Brush backgroundColor { get; set; }
+        public int timeOut { get; set; }
+    }
+}
